d4c002c9330ac89e6fb66d70cf6e12542e0ff3d7
Bryan Oyloe <boyloe@gmail.com>
2020-06-22 17:28:02 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,8 +3,10 @@
 
 def key_for_min_value(name_hash)
   lowest_key = ""
-  lowest_value = 0 
+  lowest_value = name_hash[0]
   name_hash.each do |key, value|
-    
-    
+    if value < lowest_value
+      lowest_key = key 
+    end  
+  end  
 end
