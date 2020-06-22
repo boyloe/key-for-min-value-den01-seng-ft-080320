1e2f73491f069943285ffc1a924a901487c48c2f
Bryan Oyloe <boyloe@gmail.com>
2020-06-22 17:38:55 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,7 +3,7 @@
 require "pry"
 def key_for_min_value(name_hash)
   lowest_key = ""
-  lowest_value = name_hash[0]
+  lowest_value = name_hash.values[0]
   binding.pry
   name_hash.each do |key, value|
     if key||value == ""
